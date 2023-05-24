c8a4259a97d68682a7aab2dc1e523b0765fad2a2
Juan Tello <jstello@gmail.com>
2023-05-24 16:04:01 -0500

    initial commit

---
+++ 1_🏞️CFRD Papers with Summaries.py
@@ -0,0 +1,125 @@
+import PyPDF2
+import os
+import pandas as pd
+import streamlit as st
+import glob
+st.title("PDF Summarization Web App")
+
+# with st.expander("About"):
+st.info(
+        """
+        This is a web app that summarizes 60+ papers on the behavior of Concrete Face Rockfill Dams.
+        On this page you can select a category and a paper from that category to view its summary.
+        On the CFRD Knowledge Base page you can ask questions about CFRDs and get answers based 
+        on the content of the papers, using an augmented version of ChatGPT. 
+        """
+        )
+    
+import os
+import PyPDF2
+def extract_images(pdf_file):
+    folder = pdf_file.split("\\")[-2]
+    file_name = pdf_file.split("\\")[-1].replace(".pdf", "")
+    pdf_file = open(pdf_file.replace('\\', os.sep), 'rb')
+    pdf_reader = PyPDF2.PdfReader(pdf_file)
+
+    # Create the images directory if it doesn't exist
+    if not os.path.exists('images'):
+        os.makedirs('images')
+
+    for page_num in range(len(pdf_reader.pages)):
+        page = pdf_reader.pages[page_num]
+        if '/XObject' in page['/Resources']:
+            x_objects = page['/Resources']['/XObject'].get_object()
+            if x_objects:
+                for obj in x_objects:
+                    if x_objects[obj]['/Subtype'] == '/Image':
+                        # Extract the image data
+                        image_data = x_objects[obj]._data
+                        # Write the image data to a file in the images directory
+                        # Check if folder exists
+                        if not os.path.exists(f"images/{folder}/{file_name}"):
+                            os.makedirs(f"images/{folder}/{file_name}")
+                        filename = f"images/{folder}/{file_name}/image_{page_num}_{obj.replace('/', '_')}.png"
+                        with open(filename, 'wb') as f:
+                            f.write(image_data)
+
+
+df = pd.read_csv("summary2.csv")
+
+if "cat_index" not in st.session_state:
+    st.session_state["cat_index"] = 0
+
+cat_index = st.session_state["cat_index"]
+
+# st.write(f"cat_index: {cat_index}")
+# st.write(f"Number of categories: {len(df['Category'].unique())}")
+# Dropdown menu keeping track of the category so that 
+# it is persistent across pages
+categories = df["Category"].unique()
+category = st.selectbox("Select a category", categories, index=int(cat_index))
+cat_index = categories.tolist().index(category)
+st.session_state["cat_index"] = cat_index
+# st.write(f"cat_index: {cat_index}")
+
+df_selected = df[df['Category'] == category]
+
+if "file_index" not in st.session_state:
+    st.session_state["file_index"] = 0
+    file_index = 0
+else:
+    file_index = st.session_state["file_index"]
+
+files = df_selected["title"].unique()    
+file = st.selectbox(
+    "Select a file from a total of " + str(len(df_selected)) + " files",
+    files,
+    index=int(file_index)
+    )
+
+file_index = files.tolist().index(file)
+st.session_state["file_index"] = file_index
+
+summary = df_selected[df_selected['title'] == file]["summary"].values[0]
+
+with st.expander("Summary"):
+    st.success(summary)
+
+pdf_file = df_selected[df_selected['title'] == file]["file_path"].values[0]
+
+# Extract images from pdf
+extract_images(pdf_file)
+
+# display the image in streamlit
+folder = pdf_file.split("\\")[-2]
+file_name = pdf_file.split("\\")[-1].replace(".pdf", "")
+
+image_files = glob.glob(f"images/{folder}/{file_name}/*.png")
+
+# Sort these files from largest to smallest
+image_files.sort(key=os.path.getsize, reverse=True)
+
+# st.write("There are " + str(len(image_files)) + " images in this paper")
+# st.write(f"len(image_files)//3 {len(image_files)//3}")
+# Display 9 images in a 3 x 3 grid
+with st.expander("All images from the paper"):
+    cols = {}
+    for i in range(len(image_files)//3):
+        cols[i] = st.columns(3)
+        for j in range(3):
+            image_i= i*3 + j
+            try:
+                cols[i][j].image(image_files[image_i])
+            except: 
+                print(f"Error displaying image {image_files[image_i]}")
+                pass
+
+st.session_state["pdf_file"] = pdf_file
+st.session_state["title"] = df_selected[df_selected['file_path'] == pdf_file]["title"].values[0]
+st.session_state["summary"] = summary
+
+file_path = r"https://github.com/jstello/streamlit-langchain/blob/master/" + pdf_file.replace('\\', '/')
+file_path = file_path.replace(' ', '%20')
+
+st.markdown(f"The original file can be found [here]({file_path})")
+
---
+++ pages/2_🏗️ CFRD Knowledge Base.py
@@ -0,0 +1,78 @@
+import streamlit as st
+import pandas as pd
+import glob
+import os
+
+# list csv files in parent directory
+
+
+summaries = pd.read_csv("../summary2.csv")
+# get the base name of file_name
+
+st.title("CFRD Knowledge Base")
+st.info("""
+    I am a knowledge base with extensive information about Concrete Face Rockfill Dams (CFRDs).
+    Based on a series of scientific papers spanning the topics of 
+    * Numerical Modelling
+    * Dam Behavior
+    * Seismic Response
+    * Rockfill Behavior
+    * Structural
+        """)
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Pinecone
+from langchain.document_loaders import TextLoader
+from langchain.document_loaders import UnstructuredPDFLoader
+import os
+embeddings = OpenAIEmbeddings()
+import pinecone 
+import os
+from tqdm.autonotebook import tqdm
+# initialize pinecone
+pinecone.init(api_key="48640420-7e79-46d4-b71d-d07286818fef",
+              environment="us-central1-gcp")
+
+def pretty_print(response):
+    import textwrap
+    # Split the response by lines of max 80 characters
+    return '\n'.join(textwrap.wrap(response, 80))
+
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import RetrievalQA
+
+temperature = 0.5
+
+llm=ChatOpenAI(temperature=temperature, max_tokens=2000)
+
+docsearch = Pinecone.from_existing_index('icold', embeddings, 
+                                         )
+qa = RetrievalQA.from_chain_type(
+    llm=llm,
+#     chain_type="map_reduce",
+    chain_type="stuff",
+    retriever=docsearch.as_retriever(),
+    return_source_documents=True,
+    verbose=True,
+    )
+
+query = st.text_input("Ask me a question about CFRDs", "What type of damage did the Zipingpu dam endure during the 2008 earthquake?")
+@st.cache_data()
+def run_query(query):
+    result = qa({"query": query, "top_k": 5, "max_tokens": 2000})
+    return result["result"], result["source_documents"]
+
+if st.button("Ask"):
+        result, sources = run_query(query)
+        st.success(result)
+        # Print sources
+        for isource, source in enumerate(sources):
+            file_name = source.metadata['source']
+            file_name = os.path.basename(file_name)
+            base_name = file_name.split("\\")[-1]
+            i = [i for i in range(len(summaries)) if base_name in summaries.iloc[i, 0]][0]
+            title = summaries.iloc[i, 1]
+
+            with st.expander(f"[{isource+1}] " + title):
+                st.write(source.page_content.replace("\n", " "))
+
